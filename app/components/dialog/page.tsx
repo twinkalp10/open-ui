7842df9ae31082323a98557e8ae0ab7daab96a41
twinkal <twinkal.work@outlook.com>
2023-08-18 16:42:39 +0530

    bug fix

--- app/components/dialog/page.tsx
+++ app/components/dialog/page.tsx
@@ -22,7 +22,7 @@ const Page = () => {
         <DialogHeader>
           <DialogTitle>Edit profile</DialogTitle>
           <DialogDescription>
-            Make changes to your profile here. Click save when you're done.
+            Make changes to your profile here. Click save when you&apos;re done.
           </DialogDescription>
         </DialogHeader>
         <div className="flex flex-col gap-4">
